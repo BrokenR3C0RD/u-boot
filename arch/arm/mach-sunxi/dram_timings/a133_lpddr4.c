5f946071faed8ee29324b6ae21560173457b7aa7
Cody Eksal <masterr3c0rd@epochal.quest>
2024-11-04 22:03:47 -0400

    Fix addrmap bug & bankgroup detection Also fix up some debugs and fix up a comment

--- arch/arm/mach-sunxi/dram_timings/a133_lpddr4.c
+++ arch/arm/mach-sunxi/dram_timings/a133_lpddr4.c
@@ -53,40 +53,40 @@ void mctl_set_timing_params(const struct dram_para *para)
 		trddata_en = 19;
 	}
 
-	writel(tras_min | tras_max << 8 | tfaw << 16 | wr2pre << 24,
+	writel_relaxed(tras_min | tras_max << 8 | tfaw << 16 | wr2pre << 24,
 	       &mctl_ctl->dramtmg[0]);
-	writel(trc | rd2pre << 8 | txp << 16, &mctl_ctl->dramtmg[1]);
-	writel(wr2rd | rd2wr << 8 | rdlat << 16 | wrlat << 24,
+	writel_relaxed(trc | rd2pre << 8 | txp << 16, &mctl_ctl->dramtmg[1]);
+	writel_relaxed(wr2rd | rd2wr << 8 | rdlat << 16 | wrlat << 24,
 	       &mctl_ctl->dramtmg[2]);
-	writel(tmod | tmrd << 12 | tmrw << 20, &mctl_ctl->dramtmg[3]);
-	writel(trp | trrd << 8 | tccd << 16 | trcd << 24,
+	writel_relaxed(tmod | tmrd << 12 | tmrw << 20, &mctl_ctl->dramtmg[3]);
+	writel_relaxed(trp | trrd << 8 | tccd << 16 | trcd << 24,
 	       &mctl_ctl->dramtmg[4]);
-	writel(tcke | tckesr << 8 | tcksre << 16 | tcksrx << 24,
+	writel_relaxed(tcke | tckesr << 8 | tcksre << 16 | tcksrx << 24,
 	       &mctl_ctl->dramtmg[5]);
-	writel((txp + 2) | 0x20UL << 16 | 0x20UL << 24, &mctl_ctl->dramtmg[6]);
-	writel(txs_x32 | 0x10 << 8 | txsabort_x32 << 16 | txsabort_x32 << 24,
+	writel_relaxed((txp + 2) | 0x20UL << 16 | 0x20UL << 24, &mctl_ctl->dramtmg[6]);
+	writel_relaxed(txs_x32 | 0x10 << 8 | txsabort_x32 << 16 | txsabort_x32 << 24,
 	       &mctl_ctl->dramtmg[8]);
-	writel(wr2rd_s | trrd_s << 8 | 0x2 << 16, &mctl_ctl->dramtmg[9]);
-	writel(0xe0c05, &mctl_ctl->dramtmg[10]);
-	writel(0x440c021c, &mctl_ctl->dramtmg[11]);
-	writel(tmrd_pda, &mctl_ctl->dramtmg[12]);
-	writel(0xa100002, &mctl_ctl->dramtmg[13]);
-	writel(txsr, &mctl_ctl->dramtmg[14]);
+	writel_relaxed(wr2rd_s | trrd_s << 8 | 0x2 << 16, &mctl_ctl->dramtmg[9]);
+	writel_relaxed(0xe0c05, &mctl_ctl->dramtmg[10]);
+	writel_relaxed(0x440c021c, &mctl_ctl->dramtmg[11]);
+	writel_relaxed(tmrd_pda, &mctl_ctl->dramtmg[12]);
+	writel_relaxed(0xa100002, &mctl_ctl->dramtmg[13]);
+	writel_relaxed(txsr, &mctl_ctl->dramtmg[14]);
 
 	clrsetbits_le32(&mctl_ctl->init[0], 0xc0000fff, 1008);
 
 	if (tpr13_flag2)
-		writel(0x420000, &mctl_ctl->init[1]);
+		writel_relaxed(0x420000, &mctl_ctl->init[1]);
 	else
-		writel(0x1f20000, &mctl_ctl->init[1]);
+		writel_relaxed(0x1f20000, &mctl_ctl->init[1]);
 
 	clrsetbits_le32(&mctl_ctl->init[2], 0xff0f, 0xd05);
-	writel(0, &mctl_ctl->dfimisc);
+	writel_relaxed(0, &mctl_ctl->dfimisc);
 
-	writel(para->mr1 << 16 | para->mr2, &mctl_ctl->init[3]);
-	writel(para->mr3 << 16, &mctl_ctl->init[4]);
-	writel(para->mr11 << 16 | para->mr12, &mctl_ctl->init[6]);
-	writel(para->tpr1 << 16 | para->mr14, &mctl_ctl->init[7]);
+	writel_relaxed(para->mr1 << 16 | para->mr2, &mctl_ctl->init[3]);
+	writel_relaxed(para->mr3 << 16, &mctl_ctl->init[4]);
+	writel_relaxed(para->mr11 << 16 | para->mr12, &mctl_ctl->init[6]);
+	writel_relaxed(para->tpr1 << 16 | para->mr14, &mctl_ctl->init[7]);
 
 	clrsetbits_le32(&mctl_ctl->rankctl, 0xff0, 0x660);
 	if (!tpr13_flag3) {
@@ -94,9 +94,9 @@ void mctl_set_timing_params(const struct dram_para *para)
 		trddata_en -= 1;
 	}
 
-	writel(tphy_wrlat | trddata_en << 16 | 0x808000 | 0x2000000,
+	writel_relaxed(tphy_wrlat | trddata_en << 16 | 0x808000 | 0x2000000,
 	       &mctl_ctl->dfitmg0);
-	writel(0x100202, &mctl_ctl->dfitmg1);
+	writel_relaxed(0x100202, &mctl_ctl->dfitmg1);
 
-	writel(trfc_min | trefi_x32 << 16, &mctl_ctl->rfshtmg);
+	writel_relaxed(trfc_min | trefi_x32 << 16, &mctl_ctl->rfshtmg);
 }
